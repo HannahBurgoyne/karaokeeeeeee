4b23950deb89d5c75c8d43b454aeef0c430dfe27
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2025-04-09 15:49:37 +1200

    clean up

--- client/components/Queue.tsx
+++ client/components/Queue.tsx
@@ -9,11 +9,15 @@ export default function Queue({ queue, currentIndex }: Props) {
   return (
     <div>
       <h2 className="text-xl font-semibold mb-2">Up Next</h2>
-      <ol className="list-decimal list-inside">
-        {queue.slice(currentIndex + 1).map((video, index) => (
-          <li key={`${video.id} ${index}`}>{video.name}</li>
-        ))}
-      </ol>
+      <ul className="list-inside">
+        {queue[currentIndex + 1] ? (
+          <li key={queue[currentIndex + 1].id}>
+            {queue[currentIndex + 1].name}
+          </li>
+        ) : (
+          <li>No more songs in the queue</li>
+        )}
+      </ul>
     </div>
   )
 }
